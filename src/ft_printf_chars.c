f6e2d8f3ae7ea6bfdd239fe267f9cf3c655b9c9e
Tom ZHOU <tzhou@e1z2r5p47.42.us.org>
2017-07-14 20:11:38 -0700

    temporary hacky handling of char tests...

--- src/ft_printf_chars.c
+++ src/ft_printf_chars.c
@@ -6,7 +6,7 @@
 /*   By: tzhou <tzhou@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/13 17:24:54 by tzhou             #+#    #+#             */
-/*   Updated: 2017/07/14 18:40:39 by tzhou            ###   ########.fr       */
+/*   Updated: 2017/07/14 20:11:03 by tzhou            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int	print_percent(t_print *env)
 	env->out = (char*)malloc(sizeof(char) * (2));
 	env->out[0] = '%';
 	env->out[1] = '\0';
+	ft_putstr(env->out);
 	env->count = 1;
 	return (0);
 }
